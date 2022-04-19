f387926933d8f1d2b35c5a928ae1ab6bd44da805
Olivier Nimal <olivier.nimal@gmail.com>
2022-04-18 23:05:26 -0400

    Bio-01

--- frontend/src/Bio.js
+++ frontend/src/Bio.js
@@ -1,12 +1,62 @@
 import styled from "styled-components";
 
+import helene_img from "../src/assets/helene.jpg"
+import katia_img from "../src/assets/katia.jpg"
+import plante from "../src/assets/plante-blanche2.png"
+import plante2 from "../src/assets/plante-blanche.png"
+
 const Bio = () => {
 
     return (
         <Wrapper>
             <BioWrapper>
                 <BioArea>
-
+                    <Bio01Wrapper>
+                        <Bio01ImageWrapper>
+                            <Bio01Image src={helene_img}/>
+                        </Bio01ImageWrapper>
+                        <Bio01Separator>
+                            <Bio01SeparatorImage src={plante}/>
+                        </Bio01Separator>
+                        <Bio01TextWrapper>
+                            <Bio01Name>Hélène Blat</Bio01Name>
+                            <Bio01Text>
+                            Passionnée depuis son plus jeune âge par les massages,<br></br>
+                            Hélène a choisi de se professionnaliser en se formant à l’école Fleur de Peau.<br></br><br></br> 
+                            La diversité de ses techniques telles que le Suédois, le Californien, l’Oriental, le Lomi-Lomi 
+                            ainsi que le thérapeutique lui permettent de proposer un massage personnalisé et unique 
+                            qui favorise l’équilibre du corps et de l’esprit.<br></br><br></br>
+                            Elle combine les bienfaits de ses techniques à l’aide de mouvements fluides, 
+                            de points de pression, d’étirements pour favoriser l’amplitude des tissus musculaires 
+                            et une profonde relaxation.<br></br><br></br>
+                            Elle utilise ses mains comme des instruments pour faire circuler les énergies 
+                            et ainsi amener à l’harmonisation de l’être dans son ensemble.<br></br><br></br>
+                            Membre du RMPQ
+                            </Bio01Text>
+                        </Bio01TextWrapper>
+                    </Bio01Wrapper>
+                    
+                    <Bio02Wrapper>
+                        <Bio02ImageWrapper>
+                            <Bio02Image src={katia_img}/>
+                        </Bio02ImageWrapper>
+                        <Bio02Separator>
+                            <Bio02SeparatorImage src={plante2}/>
+                        </Bio02Separator>
+                        <Bio02TextWrapper>
+                            <Bio02Name>Katia Breton</Bio02Name>
+                            <Bio02Text>
+                            Katia Breton est massothérapeute agréée certifiée, ainsi que réflexologue.<br></br><br></br>
+                            Ses techniques sont le suédois, la fasciathérapie, la réflexologie,<br></br>
+                            la thérapie crânienne et le massage pour personnes âgées.<br></br><br></br>
+                            Elle pratique depuis 2009.
+                            Pour Katia, la massothérapie est synonyme de mouvement, créant plus de souplesse dans les tissus tout en activant le système parasympathique.<br></br><br></br>
+                            L’écoute ainsi que l’intention dans ses mains lui permettent d’accompagner la personne vers un meilleur équilibre et bien-être.<br></br><br></br>
+                            Elle travaille en respectant chaque personne dans sa globalité.<br></br><br></br>
+                            Membre de la FQM
+                            </Bio02Text>
+                        </Bio02TextWrapper>
+                    </Bio02Wrapper>                    
                 </BioArea>
             </BioWrapper>
         </Wrapper>
@@ -22,24 +72,124 @@ const Wrapper = styled.div`
 `
 
 const BioWrapper = styled.div`
-
     height: 85vh;
     width: 98vw;
     margin-top: 0px;
     border-radius: 0px 0px 15px 15px;
     background-color: white;
-    box-shadow: 1px 8px 8px #888888;
+    box-shadow: 1px 8px 8px #888888;    
+`
+const BioArea = styled.div`    
+    height: 80vh;
     display: grid;
-    grid-template-rows: 10% 80% 10%;
-    justify-content: center;  
-
+    grid-template-columns: 50% 50%;    
 `
-const BioArea = styled.div`
-    
-    height: 20vh;
+
+const Bio01Wrapper = styled.div`    
+    background-color: #7e9e6c;
+    height: 75vh;
+    width: 35vw;
+    justify-self: center;
+    align-self: center;
+    display: grid;
+    grid-template-rows: 40% 15% 40%;
+`
+const Bio01ImageWrapper = styled.div`    
+    height: 100%;
+    width: 100%;
+    justify-self: center;
+    align-self: center;
     display: flex;
-    flex-direction: column;
-    border: solid 1px black;
-
+    justify-content: center;
+    align-items: center;
 `
+const Bio01Image = styled.img`
+    width: 50%;
+    border-radius: 5px;    
+`
+
+const Bio01Separator = styled.div`    
+    height: 100%;
+    width: 100%;
+    justify-self: center;
+    align-self: center;
+    display: flex;
+    justify-content: center;
+`
+const Bio01SeparatorImage = styled.img`
+    width: 30%;
+`
+const Bio01TextWrapper = styled.div`    
+    height: 100%;
+    width: 90%;
+    justify-self: center;
+    align-self: center;
+`
+
+const Bio01Name = styled.p`
+    font-size: 24px;
+    color: white;
+    font-variant-caps: small-caps;
+    padding-bottom: 10px;
+    text-align: center;
+`
+const Bio01Text = styled.p`
+    font-size: 16px;
+    color: white;    
+`
+
+const Bio02Wrapper = styled.div`    
+    background-color: #7e9e6c;
+    height: 75vh;
+    width: 35vw;
+    justify-self: center;
+    align-self: center;
+    display: grid;
+    grid-template-rows: 40% 15% 40%;   
+`
+
+const Bio02ImageWrapper = styled.div`    
+    height: 100%;
+    width: 100%;
+    justify-self: center;
+    align-self: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const Bio02Image = styled.img`
+    width: 50%;
+    border-radius: 5px;    
+`
+
+const Bio02Separator = styled.div`    
+    height: 100%;
+    width: 100%;
+    justify-self: center;
+    align-self: center;
+    display: flex;
+    justify-content: center;
+`
+const Bio02SeparatorImage = styled.img`
+    width: 30%;
+`
+const Bio02TextWrapper = styled.div`    
+    height: 100%;
+    width: 90%;
+    justify-self: center;
+    align-self: center;
+`
+
+const Bio02Name = styled.p`
+    font-size: 24px;
+    color: white;
+    font-variant-caps: small-caps;
+    padding-bottom: 10px;
+    text-align: center;
+`
+const Bio02Text = styled.p`
+    font-size: 16px;
+    color: white;    
+`
+
 export default Bio;
